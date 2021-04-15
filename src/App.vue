d685dcdd2287378c8354e9b5481ba241dd890135
songjiwei <songjiwei@BANKQH-DSF242.bankqh.com>
2021-04-15 23:25:47 +0800

    feat: #
    功能描述:
    
    影响范围:

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">© 2021 知乎专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+const currentUser: UserProps = {
+  isLogin: false,
+  name: 'Luckycc'
+}
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    GlobalHeader
+  },
+  setup () {
+    return {
+      currentUser
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,45 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createStore } from 'vuex'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import ColumnDetail from './views/ColumnDetail.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const store = createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add (state) {
+      state.count++
+    }
+  }
+})
+console.log('store', store.state.count)
+store.commit('add')
+console.log('store', store.state.count)
+const routerHistory = createWebHistory()
+const router = createRouter({
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/column/:id',
+      name: 'column',
+      component: ColumnDetail
+    }
+  ]
+})
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
